4f4b62d7ce557ea47532b3a63a291bb71fe748ad
Abogeerick <abogeerick@gmail.com>
2023-08-21 20:10:20 +0300

    Complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed = None):
+        self._name = None
+        self._breed = None
+
+        if breed is not None:
+            self.set_breed(breed)
+        
+        if name is not None:
+            self.set_name(name)
+
+    
+    def get_name(self):
+        return self._name 
+    
+    def get_breed(self):
+        return self._breed
+    
+    
+    def set_name(self, value):
+        if value is None:
+            self._name = None
+        elif isinstance(value, str) and (1 <= len(value) <= 25):
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def set_breed(self,value):
+        if value is None:
+            self._breed = None
+        elif value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
