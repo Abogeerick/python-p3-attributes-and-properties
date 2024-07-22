fe94da077e7f801d9debb58b71e0210ca472dc59
Abogeerick <abogeerick@gmail.com>
2024-07-22 08:42:05 +0300

    Completed assignment

--- lib/person.py
+++ lib/person.py
@@ -1,49 +1,24 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    def __init__(self, name = None, job = None):
-        self._name = None
-        self._job = None
-        
-        if name is not None:
-            self.set_name(name)
-
-        if job is not None:
-            self.set_job(job)
-
+    def __init__(self, name="Any", job="Any") -> None:
+        self.name = name
+        self.job = job 
 
     def get_name(self):
-        return self._name
+        return self._name 
     
     def get_job(self):
         return self._job
     
-    def set_name(self, value):
-        if value is None:
-            self._name = None
-        elif isinstance(value, str) and (len(value) >= 1 and len(value) < 25):
-            self._name = value.title()
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) < 25:
+            self._name = name.title()
         else:
             print("Name must be string between 1 and 25 characters.")
-    
-    def set_job(self, value):
-        if value in APPROVED_JOBS:
-             self._job = value
+
+    def set_job(self, job):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+        if job in approved_jobs:
+            self._job = job
         else:
             print("Job must be in list of approved jobs.")
 
